d0d0748088daa220dab9b64205338faf8acbdc79
songkaiping <songkaiping@landsone.com>
2018-09-11 23:07:26 +0800

    viper 模板

---
+++ Viperit/Protocol-oriented Module.xctemplate/Universal/___FILEBASENAME___ModuleApi.swift
@@ -0,0 +1,26 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Viperit
+
+//MARK: - ___VARIABLE_ViperitModuleName___Router API
+protocol ___VARIABLE_ViperitModuleName___RouterApi: RouterProtocol {
+}
+
+//MARK: - ___VARIABLE_ViperitModuleName___View API
+protocol ___VARIABLE_ViperitModuleName___ViewApi: UserInterfaceProtocol {
+    func prepare()
+}
+
+//MARK: - ___VARIABLE_ViperitModuleName___Presenter API
+protocol ___VARIABLE_ViperitModuleName___PresenterApi: PresenterProtocol {
+}
+
+//MARK: - ___VARIABLE_ViperitModuleName___Interactor API
+protocol ___VARIABLE_ViperitModuleName___InteractorApi: InteractorProtocol {
+}
